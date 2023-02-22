0e8d1a7da28b011897fb926351508087ebe92318
SaarthChat723 <saarth.chat@gmail.com>
2023-02-21 20:17:31 -0500

    Push

--- lab-insta-parse/AppDelegate.swift
+++ lab-insta-parse/AppDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 // TODO: Pt 1 - Import Parse Swift
+import ParseSwift
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,10 +19,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         // TODO: Pt 1 - Initialize Parse SDK
         // https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
+        ParseSwift.initialize(applicationId: "RbnviQ3OKqRxnjRAcYyWistPqS4aK1Q6xLZCPYlJ",
+                              clientKey: "br6iKurariY2qLoidHVuYbasEXojWy2OTTVq8z41",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
 
 
         // TODO: Pt 1: - Instantiate and save a test parse object to your server
         // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L121
+        // Instantiate the test parse object
+//        var score = GameScore()
+//        score.playerName = "Kingsley"
+//        score.points = 13
+//
+//        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+//        // If no callbackQueue is specified it returns to main queue.
+//        score.save { result in
+//            switch result {
+//            case .success(let savedScore):
+//                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+//            case .failure(let error):
+//                assertionFailure("Error saving: \(error)")
+//            }
+//        }
 
 
         return true
@@ -43,5 +63,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 // TODO: Pt 1 - Create Test Parse Object
 // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
-
+// Create your own value type `ParseObject`.
+//struct GameScore: ParseObject {
+//    // These are required by ParseObject
+//    var objectId: String?
+//    var createdAt: Date?
+//    var updatedAt: Date?
+//    var ACL: ParseACL?
+//    var originalData: Data?
+//
+//    // Your own custom properties.
+//    // All custom properties must be optional.
+//    var playerName: String?
+//    var points: Int?
+//}
+//
+//// Sample Usage
+////
+//// var score = GameScore()
+//// score.playerName = "Kingsley"
+//// score.points = 13
+//
+//// OR Implement a custom initializer (OPTIONAL i.e. NOT REQUIRED)
+//// It's recommended to place custom initializers in an extension
+//// to preserve the memberwise initializer.
+//extension GameScore {
+//
+//    // Use the init to set your custom properties
+//    // NOTE: Properties in custom init are NOT required to be optional
+//    init(playerName: String, points: Int) {
+//        self.playerName = playerName
+//        self.points = points
+//    }
+//}
+//
+//// Sample Usage
+////
+//// let score = GameScore(playerName: "Kingsley", points: 13)
 
--- lab-insta-parse/Login/LoginViewController.swift
+++ lab-insta-parse/Login/LoginViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 // TODO: Pt 1 - Import Parse Swift
-
+import ParseSwift
 
 class LoginViewController: UIViewController {
 
@@ -32,7 +32,20 @@ class LoginViewController: UIViewController {
         }
 
         // TODO: Pt 1 - Log in the parse user
+        // Log in the parse user
+        User.login(username: username, password: password) { [weak self] result in
 
+            switch result {
+            case .success(let user):
+                print("✅ Successfully logged in as user: \(user)")
+
+                // Post a notification that the user has successfully logged in.
+                NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+
+            case .failure(let error):
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
     }
 
     private func showAlert(description: String?) {
